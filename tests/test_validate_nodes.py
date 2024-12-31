8718712f14e4c37d4966334d5178e9ad1ebdcfd1
Robin Huang <robin.j.huang@gmail.com>
2024-12-30 16:38:18 -0800

    Add more test cases.

--- tests/test_validate_nodes.py
+++ tests/test_validate_nodes.py
@@ -13,12 +13,30 @@ class ReorderedNode:
 class IncompatibleNode:
     RETURN_TYPES = ("STRING", "BOOL", "FLOAT")
 
+class AddTypeNode:
+    RETURN_TYPES = ("STRING", "INT", "FLOAT", "BOOL")
+
+class RemoveTypeNode:
+    RETURN_TYPES = ("STRING", "INT")
+
 class NoReturnTypesNode:
     pass
 
 def test_compare_return_types_no_changes():
     changes = compare_return_types("TestNode", BaseNode, PRNode)
     assert len(changes) == 0
+
+
+def test_compare_return_types_add_type():
+    changes = compare_return_types("TestNode", BaseNode, AddTypeNode)
+    assert len(changes) == 0
+
+def test_compare_return_types_remove_type():
+    changes = compare_return_types("TestNode", BaseNode, RemoveTypeNode)
+    assert len(changes) == 1
+    assert changes[0].change_type == BreakingChangeType.RETURN_TYPES_CHANGED
+    assert changes[0].base_value == ("STRING", "INT", "FLOAT")
+    assert changes[0].pr_value == ("STRING", "INT")
 
 def test_compare_return_types_reordered():
     changes = compare_return_types("TestNode", BaseNode, ReorderedNode)
